99858a9da7fc9e8b7dc80c9f8137ad745c61b2ae
hyunjinnnnnp <imhjinnnnn@gmail.com>
2022-02-18 16:44:17 +0900

    STUN server for getting public IP

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -58,7 +58,20 @@ const handlePeerTrack = (data) => {
 };
 
 const makeConnection = () => {
-  myPeerConnection = new RTCPeerConnection();
+  myPeerConnection = new RTCPeerConnection({
+    // STUN server for getting public IP
+    iceServers: [
+      {
+        urls: [
+          "stun:stun.l.google.com:19302",
+          "stun:stun1.l.google.com:19302",
+          "stun:stun2.l.google.com:19302",
+          "stun:stun3.l.google.com:19302",
+          "stun:stun4.l.google.com:19302",
+        ],
+      },
+    ],
+  });
   myPeerConnection.addEventListener("icecandidate", handleIce);
   myPeerConnection.addEventListener("track", handlePeerTrack);
   myStream
